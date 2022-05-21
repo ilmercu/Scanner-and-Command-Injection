6881364e005038448b4739d5065d26c77120d152
ilmercu <mercuri.ndr@gmail.com>
2022-05-21 16:36:08 +0200

    code refactor

--- main.py
+++ main.py
@@ -1,67 +1,66 @@
 from config import *
 import requests
 
-requestsDict = [ ]
+def elaborate_response(response):
+    if DEBUG:
+        print('\n[DEBUG] - URL')
+        print(f'{response.url}')
+        
+        print('[DEBUG] - REQUEST RESULT')
+        print(f'{response.text}')
 
 # read requests details
-with open(REQUESTS_INPUT_PATH) as f:
-    for line in f:
-        values = line.strip().split(REQUESTS_SPLIT_VAL)
+def read_requests_details(requestsDict):
+    with open(REQUESTS_INPUT_PATH) as f:
+        for line in f:
+            values = line.strip().split(REQUESTS_SPLIT_VAL)
 
-        requestsDict.append({ 
-            'method':  values[0],
-            'url': values[1],
-            'parameters': values[2].split(REQUESTS_PARAMETERS_SPLIT_VAL),
-        })
+            requestsDict.append({ 
+                'method':  values[0],
+                'url': values[1],
+                'parameters': values[2].split(REQUESTS_PARAMETERS_SPLIT_VAL),
+            })
 
 # read payloads to inject
-with open(PAYLOADS_INPUT_PATH) as f:
-    i = 0
+def read_payloads(requestsDict):
+    with open(PAYLOADS_INPUT_PATH) as f:
+        i = 0
 
-    for line in f:
-        payloads = line.strip().split(PAYLOADS_SPLIT_VAL)
+        for line in f:
+            payloads = line.strip().split(PAYLOADS_SPLIT_VAL)
+            
+            requestsDict[i]['payloads'] = payloads
+
+            i += 1
+
+def send_request(requestsDict):
+    for request in requestsDict:
+        final_url = TARGET + request['url']
         
-        requestsDict[i]['payloads'] = payloads
-
-        i += 1
-
-if DEBUG:
-    print('[DEBUG] - REQUESTS DICTIONARY')
-    print(requestsDict)
-
-for request in requestsDict:
-    final_url = TARGET + request['url']
-    
-    for payload in request['payloads']:
-        if 'GET' == request['method'].upper():
-            payload_injection = ''
+        for payload in request['payloads']:
+            data = { }
 
             for parameter in request['parameters']:
-                payload_injection += parameter + '=' + payload
+                data[parameter] = payload
             
-            if DEBUG:
-                print('\n[DEBUG] - URL - GET METHOD')
-                print(f'{final_url}?{payload_injection}')
-            
-            r = requests.get(final_url + '?' + payload_injection)
+            if 'GET' == request['method'].upper():
+                elaborate_response(requests.get(final_url, params=data))
+            elif 'POST' == request['method'].upper():
+                elaborate_response(requests.post(final_url, data=data))
+            else:
+                print(f'Method {request["method"]} is not supported')
 
-            if DEBUG:
-                print('[DEBUG] - REQUEST RESULT')
-                print(f'{r.text}')
-        elif 'POST' == request['method'].upper():
-            post_data = { }
+def main():
+    requestsDict = [ ]
 
-            for parameter in request['parameters']:
-                post_data[parameter] = payload
-            
-            if DEBUG:
-                print('\n[DEBUG] - URL - POST METHOD')
-                print(f'{final_url}')
-            
-            r = requests.post(final_url, data=post_data)
+    read_requests_details(requestsDict)
+    read_payloads(requestsDict)
 
-            if DEBUG:
-                print('[DEBUG] - REQUEST RESULT')
-                print(f'{r.text}')
-        else:
-            print(f'Method {request["method"]} is not supported')
+    if DEBUG:
+        print('[DEBUG] - REQUESTS DICTIONARY')
+        print(requestsDict)
+
+    send_request(requestsDict)
+
+if __name__=="__main__":
+    main()
