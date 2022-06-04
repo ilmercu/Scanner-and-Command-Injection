676a51f02a7c1e9648ae182db4bfbb2f6c5defff
ilmercu <mercuri.ndr@gmail.com>
2022-06-04 10:50:51 +0200

    added xss target files

---
+++ xss-target/echo-multi-attr.php
@@ -0,0 +1,5 @@
+<?php
+
+$name = $_GET['name'];
+echo "Welcome <span id='test-id' class='$name' onclick=''>user</span>!";
+
---
+++ xss-target/echo-multi-class.php
@@ -0,0 +1,5 @@
+<?php
+
+$name = $_GET['name'];
+echo "Welcome <span id='test-id' class='test-class1 $name test-class2' onclick=''>user</span>!";
+
